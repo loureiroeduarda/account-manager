86f108af0141dc4b5378fb7ddd761de0250758dc
loureiroeduarda <adv.eduardaloureiro@gmail.com>
2023-06-12 16:43:27 -0300

    Method created to display the summary of accounts

--- src/com/github/loureiroeduarda/model/Account.java
+++ src/com/github/loureiroeduarda/model/Account.java
@@ -47,7 +47,7 @@ public class Account {
 
     @Override
     public String toString() {
-        return "Código do banco: " + bankCode + "Número da agência:" + agencyNumber + "Número da conta:"
-                + accountNumber + "Saldo da conta: " + accountBalance;
+        return "Código do banco: " + bankCode + " | Número da agência:" + agencyNumber + " | Número da conta:"
+                + accountNumber + " | Saldo da conta: " + accountBalance;
     }
 }
